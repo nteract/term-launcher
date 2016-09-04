f9631f6639cc485f2721736c60fe79a8a7e5cf27
Lukas Geiger <lukas.geiger94@gmail.com>
2016-09-04 22:00:26 +0200

    Initial Commit.
    Tested on Mac. May or may not work on windows and linux.

---
+++ README.md
@@ -0,0 +1,5 @@
+# term-launcher
+
+Launch terminals and jupyter consoles from node.
+
+This isn't tested on Linux and Windows if you can please report if it works.
---
+++ index.js
@@ -0,0 +1,84 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+const commandExists = require('command-exists');
+const platform = process.platform;
+
+function launchTerminal(command, cwd, terminal) {
+  if (platform == 'darwin') {
+    launchDarwinTerminal(command, cwd, terminal);
+  } else if (platform == 'linux') {
+    launchLinuxTerminal(command, cwd, terminal);
+  } else if (platform == 'win32') {
+    launchWindowsTerminal(command, cwd, terminal);
+  } else {
+    throw Error('Only Linux, OS X and Windows are supported');
+  }
+}
+
+function launchDarwinTerminal(command, cwd, terminal = 'Terminal.app') {
+  var scriptPath = 'cmd-script.sh';
+  var script;
+  if (cwd === undefined || cwd === null || cwd === '') {
+    script = `#!/bin/bash\n${command}`;
+  } else {
+    script = `#!/bin/bash\ncd ${cwd}\n${command}`;
+  }
+
+  var cmd = `open -a ${terminal} ${scriptPath}`;
+
+  fs.writeFile(scriptPath, script, (err) => {
+    if (err) throw err;
+    fs.chmod(scriptPath, '755', (err) => {
+      if (err) throw err;
+      exec(cmd, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+function launchLinuxTerminal(command, cwd, terminal = 'gnome-terminal') {
+  var cmd;
+  if (cwd === undefined || cwd === null || cwd === '') {
+    cmd = `${terminal} -e ${command}`;
+  } else {
+    cmd = `${terminal} -e cd ${cwd}; ${command};`;
+  }
+
+  exec(cmd, (err) => { if (err) throw err; });
+}
+
+function launchWindowsTerminal(command, cwd, terminal = 'cmd') {
+  var cmd;
+  if (cwd === undefined || cwd === null || cwd === '') {
+    cmd = `start ${terminal} /k ${command}`;
+  } else {
+    cmd = `start ${terminal} /k cd ${cwd}; ${command};`;
+  }
+
+  exec(cmd, (err) => { if (err) throw err; });
+}
+
+function launchJupyter(connectionFile, cwd, terminal) {
+  var args = ` console --existing ${connectionFile}`;
+  commandExists('jupyter', function(err, exist) {
+    if (err) throw err;
+    if(exist) {
+      launchTerm('jupyter' + args, cwd, terminal);
+    } else {
+      commandExists('ipython', function(err, exist) {
+        if (err) throw err;
+        if(exist) {
+          launchTerm('ipython' + args, cwd, terminal);
+        } else {
+          throw Error('Could not find `jupyter` or `ipython`.');
+        }
+      });
+    }
+  });
+}
+
+module.exports = {
+  launchTerminal,
+  launchJupyter
+};
