b0acf98ba41a1d854997248016602bf4a9b1adff
Lukas Geiger <lukas.geiger94@gmail.com>
2016-09-08 16:59:02 +0200

    Identify private functions with leading underscore.
    They are exported for testing purpose.

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ function launchTerminal(command, cwd, terminal = getDefaultTerminal(),
 
   if (!terminal) return callback(Error('Could not find the terminal.'));
   if (platform == 'darwin') {
-    getDarwinCommand(command, cwd, terminal, (err, cmd) => {
+    _getDarwinCommand(command, cwd, terminal, (err, cmd) => {
       if (err) return callback(err);
       exec(cmd, callback);
     });
   } else if (platform == 'linux') {
-    exec(getLinuxCommand(command, cwd, terminal), callback);
+    exec(_getLinuxCommand(command, cwd, terminal), callback);
   } else if (platform == 'win32') {
-    exec(getWindowsCommand(command, cwd, terminal), callback);
+    exec(_getWindowsCommand(command, cwd, terminal), callback);
   } else {
     callback(Error('Only Linux, OS X and Windows are supported'));
   }
 }
 
-function getDarwinCommand(command, cwd, terminal, callback) {
+function _getDarwinCommand(command, cwd, terminal, callback) {
   var cmd = `open -a ${terminal} `;
 
   if (!command) {
@@ -32,7 +32,7 @@ function getDarwinCommand(command, cwd, terminal, callback) {
     callback(null, cmd);
   } else {
     var scriptPath = path.join(__dirname, 'cmd-script.sh');
-    var script = `#!/bin/bash\n${joinCommands(cwd, command, '\n')}\n/bin/bash`;
+    var script = `#!/bin/bash\n${_joinCommands(cwd, command, '\n')}\n/bin/bash`;
 
     fs.writeFile(scriptPath, script, (err) => {
       if (err) return callback(err);
@@ -44,17 +44,17 @@ function getDarwinCommand(command, cwd, terminal, callback) {
   }
 }
 
-function getLinuxCommand(command, cwd, terminal) {
+function _getLinuxCommand(command, cwd, terminal) {
   // http://askubuntu.com/questions/484993/run-command-on-anothernew-terminal-window
-  var commands = joinCommands(cwd, command, '; ');
+  var commands = _joinCommands(cwd, command, '; ');
   if (commands) {
     return `${terminal} -e "bash -c \\"${commands}; exec bash\\""`;
   }
   return terminal;
 }
 
-function getWindowsCommand(command, cwd, terminal) {
-  return `start ${terminal} /k "${joinCommands(cwd, command, ' & ')}"`;
+function _getWindowsCommand(command, cwd, terminal) {
+  return `start ${terminal} /k "${_joinCommands(cwd, command, ' & ')}"`;
 }
 
 function launchJupyter(connectionFile, cwd, jupyterConsole = 'console',
@@ -106,7 +106,7 @@ function getDefaultTerminal() {
   }
 }
 
-function joinCommands(cwd, cmd, delimiter) {
+function _joinCommands(cwd, cmd, delimiter) {
   var cmds = [];
   if (cwd) {
     cmds.push(`cd ${cwd}`);
@@ -123,8 +123,8 @@ module.exports = {
   launchTerminal,
   launchJupyter,
   getDefaultTerminal,
-  joinCommands,
-  getDarwinCommand,
-  getLinuxCommand,
-  getWindowsCommand
+  _joinCommands,
+  _getDarwinCommand,
+  _getLinuxCommand,
+  _getWindowsCommand
 };
